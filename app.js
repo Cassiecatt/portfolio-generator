dffe695111b680a24f84b26032b82062bce96ffe
Cassiecatt <65003865+Cassiecatt@users.noreply.github.com>
2020-07-20 20:06:35 -0700

    commented out console.log

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ console.log(profileDataArgs);
 //Notice the lack of parentheses around the 'profileDataArr' parameter?
 const printProfileData = profileDataArr => {
     for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
+    //   console.log(profileDataArr[i]);
         console.log('=============');
 
     profileDataArr.forEach(profileItem => console.log(profileItem));
