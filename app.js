b9521d2295aced1549776bd2ae5d929f8aced649
Cassiecatt <65003865+Cassiecatt@users.noreply.github.com>
2020-07-23 16:57:12 -0700

    added validation to questions

--- app.js
+++ app.js
@@ -20,12 +20,27 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name?'
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your name!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'github',
-            message: 'Enter your Github Username'
+            message: 'Enter your Github Username (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your Github Username');
+                }
+            }
         },
         {
             type: 'input',
@@ -52,12 +67,26 @@ if (!portfolioData.projects) {
         {
             type: 'input',
             name: 'name',
-            message: 'What is the name of your project?'
+            message: 'What is the name of your project? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your project name');
+                }
+            }
         },
         {
             type: 'input',
             name: 'description',
-            message: 'Provide a desription of the project (Required)'
+            message: 'Provide a desription of the project (Required)',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your project description');
+                }
+            }
         },
         {
             type: 'checkbox',
@@ -68,7 +97,14 @@ if (!portfolioData.projects) {
           {
             type: 'input',
             name: 'link',
-            message: 'Enter the GitHub link to your project. (Required)'
+            message: 'Enter the GitHub link to your project. (Required)',
+            validate: linkInput => {
+                if (linkInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your Github link to your project');
+                }
+            }
           },
           {
             type: 'confirm',
