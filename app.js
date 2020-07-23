6e255013927e693a34ce65d36e144f75ff7dd969
Cassiecatt <65003865+Cassiecatt@users.noreply.github.com>
2020-07-22 17:43:22 -0700

    installed and used inquirer

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+// // const fs = require('fs');
+// // const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2);
+// const profileDataArgs = process.argv.slice(2);
 
-// Same as - const name = profileDataArgs[0]; & const github = profileDataArgs[1];
-const [name, github] = profileDataArgs;
+// // Same as - const name = profileDataArgs[0]; & const github = profileDataArgs[1];
+// const [name, github] = profileDataArgs;
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error (err);
+// // fs.writeFile('./index.html', generatePage(name, github), err => {
+// //     if (err) throw new Error (err);
 
-    console.log('Portfolio complete! Check out the index.html to see the output!');
+// //     console.log('Portfolio complete! Check out the index.html to see the output!');
 
-});
+// // });
 
+const inquirer = require('inquirer');
 
-
+inquirer
+.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }
+])
+.then(answers => console.log(answers));
